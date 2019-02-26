76918ea9a67cc2c6037d99e352bb070d17d91464
Ochaun <ochaun@secureideas.com>
2019-02-26 12:22:48 -0500

    Added single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
   render() {
     return (
       <>
@@ -18,16 +14,10 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
-        </button>
-        <button
-          onCLick={this.handleReset}
-          className="btn btn-primary btn-sm m-2"
-        >
-          Reset
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
@@ -35,8 +25,6 @@ class Counter extends Component {
         >
           Delete
         </button>
-        {/* {this.state.tags.length === 0 && "Please Create a new tags"}
-        {this.renderTags()}*/}
       </>
     );
   }
@@ -48,34 +36,18 @@ class Counter extends Component {
     this.handleIncrement.bind(this);
   }
 */
-  handleIncrement = () => {
-    this.setState({ count: this.state.value + 1 });
-  };
-
-  //Conditional Rendering
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   //Dynamically renderd css
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
     // changing state attribute
-    const { value: count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "zero" : value;
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -17,10 +17,19 @@ export default class counters extends Component {
   };
 
   handleReset = () => {
-    this.state.counters.map(c => {
+    const counters = this.state.counters.map(c => {
       c.value = 0;
       return c;
     });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    //... operator copies all of the previous elements in obj/arr
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
     this.setState({ counters });
   };
 
@@ -29,8 +38,19 @@ export default class counters extends Component {
       <>
         {/* Map every element in array to a JSX obj with a key*/}
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} counter={counter} />
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
         ))}
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
       </>
     );
   }

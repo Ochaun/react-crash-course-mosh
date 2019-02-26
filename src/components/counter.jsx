fae6b41cd0f7c1560e90e5922a2a6ccb6744ea18
Ochaun <ochaun@secureideas.com>
2019-02-26 11:19:41 -0500

    added delete functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -25,6 +25,12 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
         {/* {this.state.tags.length === 0 && "Please Create a new tags"}
         {this.renderTags()}*/}
       </>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,12 +10,23 @@ export default class counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+  handleDelete = counterId => {
+    //remove the given counter from state
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <>
         {/* Map every element in array to a JSX obj with a key*/}
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+          <Counter
+            key={counter.id}
+            id={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+          />
         ))}
       </>
     );

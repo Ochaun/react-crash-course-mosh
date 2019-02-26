f584b699f2abd14f38ac6ccd065519ff148f0223
Ochaun <ochaun@secureideas.com>
2019-02-26 10:40:01 -0500

    Passing data to Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
     //tags: ["tag1", "tag2", "tag3"]
   };
   //State stored as a seperate variable
@@ -39,7 +39,7 @@ class Counter extends Component {
   }
 */
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.value + 1 });
   };
 
   //Conditional Rendering
@@ -58,13 +58,13 @@ class Counter extends Component {
   //Dynamically renderd css
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
     // changing state attribute
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,7 +4,7 @@ import Counter from "../components/counter";
 export default class counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
@@ -15,7 +15,7 @@ export default class counters extends Component {
       <>
         {/* Map every element in array to a JSX obj with a key*/}
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} selected={true} />
         ))}
       </>
     );
